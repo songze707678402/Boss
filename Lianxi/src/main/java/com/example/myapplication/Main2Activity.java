59fcbada518336b6ca675aa04731e0bb327f6998
宋泽港 <707678402@qq.com>
2016-12-26 18:28:01 +0800

    gai Lianxi

--- Lianxi/src/main/java/com/example/myapplication/Main2Activity.java
+++ Lianxi/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent=getIntent();
+
+        String name=intent.getStringExtra("name");
+        String sex=intent.getStringExtra("sex");
+        int age=intent.getIntExtra("age",-1);
+        TextView main2_text=(TextView) findViewById(R.id.main2_text);
+        main2_text.setText(name+":"+age+":"+sex);
+
     }
 }
